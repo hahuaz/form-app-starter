200e67e2a72d7a347ccbd1d7e8b7909a4d5444cb
Hasan <dev.hahuaz@gmail.com>
2024-03-25 14:28:32 +0300

    feat: add devtools

--- src/store/count.ts
+++ src/store/count.ts
@@ -18,36 +18,63 @@ type CountActions = {
 type CountStore = CountState & CountActions;
 
 export const useCountStore = create<CountStore>()(
-  // immer middleware allows us to use mutable updates in a safe way. otherwise we would have to return a new object that deep merges the previous state with the new state. for example: set({ ...state, deep: { ...state.deep, foo: 'bar' } })
-  immer((set) => ({
-    count: 0,
-    isCalcFinished: false,
-    // payload type is inferred as number
-    setCount: (count) => {
-      set((prevState) => {
-        prevState.count = count;
-      });
-    },
-    async asyncSetCount(count) {
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      set((prevState) => {
-        prevState.count = count;
-      });
-    },
-    increment: () => {
-      set((prevState) => {
-        prevState.count += 1;
-      });
-    },
-    decrement: () => {
-      set((prevState) => {
-        prevState.count -= 1;
-      });
-    },
-    setCalcFinished: (isCalcFinished) => {
-      set((prevState) => {
-        prevState.isCalcFinished = isCalcFinished;
-      });
-    },
-  }))
+  devtools(
+    // immer middleware allows us to use mutable updates in a safe way. otherwise we would have to return a new object that deep merges the previous state with the new state.
+    // for example: set({ ...state, deep: { ...state.deep, foo: 'bar' } })
+    // but instead we can now write: set(state => { state.deep.foo = 'bar' })
+    immer((set) => ({
+      count: 0,
+      isCalcFinished: false,
+      // payload type is inferred as number
+      setCount: (count) => {
+        set(
+          (prevState) => {
+            prevState.count = count;
+          },
+          true,
+          "countStore/setCount"
+        );
+      },
+      async asyncSetCount(count) {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        set(
+          (prevState) => {
+            prevState.count = count;
+          },
+          true,
+          "countStore/asyncSetCount"
+        );
+      },
+      increment: () => {
+        set(
+          (prevState) => {
+            prevState.count += 1;
+          },
+          true,
+          "countStore/increment"
+        );
+      },
+      decrement: () => {
+        set(
+          (prevState) => ({
+            count: prevState.count - 1,
+          }),
+          true,
+          "countStore/decrement"
+        );
+      },
+      setCalcFinished: (isCalcFinished) => {
+        set(
+          (prevState) => {
+            prevState.isCalcFinished = isCalcFinished;
+          },
+          true,
+          "countStore/setCalcFinished"
+        );
+      },
+    })),
+    {
+      name: "countStore",
+    }
+  )
 );
